fd7764e4af0eb9c03ddb377c77d97c9877fecb6b
ColeTrammer <cole.trammer@gmail.com>
2020-07-10 18:54:06 -0700

    graph

--- main.js
+++ main.js
@@ -10,30 +10,20 @@ let t = 0;
 let anf = -1;
 let running = false;
 
-const hsvToRgb = (color) => {
-    const H = color[0];
-    const S = color[1];
-    const L = color[2];
-    const f = (n) => {
-        const k = (n + H / 30) % 12;
-        const a = S * Math.min(L, 1 - L);
-        return L - a * Math.max(-1, Math.min(k - 3, 9 - k, 1));
-    };
-    return [f(0) * 255, f(8) * 255, f(4) * 255];
+const visited = new Array(width * height);
+
+const pos = (t) => {
+    return [t, -Math.sin((t * 2 * Math.PI) / width) * (height / 4) + height / 2];
 };
 
 const colorAt = (x, y, t) => {
-    x -= width / 2;
-    y -= height / 2;
-    const R = Math.sqrt(x * x, y * y);
-    let Theta = Math.atan2(y, x);
-    if (Theta < 0) {
-        Theta += 2 * Math.PI;
-    }
-    return hsvToRgb([(Theta * 180) / Math.PI + t, (1.0 / R) * 75, 0.5]);
+    return visited[y * width + x] ? [0, 0, 123] : [255, 255, 255];
 };
 
 const render = () => {
+    const position = pos(t);
+    visited[Math.round(position[1]) * width + Math.round(position[0])] = 1;
+
     for (let r = 0; r < pixels.height; r++) {
         for (let c = 0; c < pixels.width; c++) {
             const pos = r * pixels.width + c;
